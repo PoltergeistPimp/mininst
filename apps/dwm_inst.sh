a298a134bbb17ab471715390594481d635330dd8
PoltergeistPimp <poltergeistpimp@proton.me>
2024-10-19 21:08:31 +0200

    add dwm install file

---
+++ apps/dwm_inst.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "################"
+echo "# DWM suckless #"
+echo "################"
+
+cd ~/.config/
+git clone https://git.suckless.org/dwm
+git clone https://git.suckless.org/dmenu
+cd ~
