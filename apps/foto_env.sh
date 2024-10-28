ed403e776f927a4a7fb694fa953c8db8b7f1b557
PoltergeistPimp <poltergeistpimp@proton.me>
2024-10-28 16:29:48 +0100

    add Dartakble, Shotwell

--- apps/foto_env.sh
+++ apps/foto_env.sh
@@ -4,10 +4,8 @@ echo "###################"
 echo "# FotografieTools #"
 echo "###################"
 
-# GIMP install met flatpak voor latest release
-if [ $fp = 1 ]; then
-  flatpak install flathub org.gimp.GIMP
-else
-  sudo apt install gimp
-fi
+# install met flatpak voor latest release
+flatpak install flathub org.gimp.GIMP
+flatpak install flathub org.darktable.Darktable 
+flatpak install flathub org.gnome.Shotwell
 
