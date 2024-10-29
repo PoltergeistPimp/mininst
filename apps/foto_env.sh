f43353fa0c6643fc0c582585b36f33ecdad48b7e
PoltergeistPimp <poltergeistpimp@proton.me>
2024-10-29 20:46:16 +0100

    add sudo, -y flag

--- apps/foto_env.sh
+++ apps/foto_env.sh
@@ -5,7 +5,7 @@ echo "# FotografieTools #"
 echo "###################"
 
 # install met flatpak voor latest release
-sudo flatpak install flathub org.gimp.GIMP
-sudo flatpak install flathub org.darktable.Darktable 
-sudo flatpak install flathub org.gnome.Shotwell
+sudo flatpak -y install flathub org.gimp.GIMP
+sudo flatpak -y install flathub org.darktable.Darktable 
+sudo flatpak -y install flathub org.gnome.Shotwell
 
