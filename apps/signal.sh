beb8b00f55ea3e1b9aeeb16a04964f1497aa0b76
PoltergeistPimp <poltergeistpimp@proton.me>
2024-12-03 02:05:01 +0100

    add -y flag

--- apps/signal.sh
+++ apps/signal.sh
@@ -12,6 +12,6 @@ echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/signal-desktop-keyring.gpg] 
   sudo tee /etc/apt/sources.list.d/signal-xenial.list
 
 # 3. Update your package database and install Signal:
-sudo apt update && sudo apt install signal-desktop
+sudo apt update && sudo apt install -y signal-desktop
 
 
